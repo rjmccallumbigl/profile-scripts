2dbac8e5c8f82c06bc2f36a020baa8f0b46e9170
Ryan McCallum <rjmccallumbigl@gmail.com>
2022-05-31 11:14:30 -0400

    Add stopping error action for try/catch

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -8,10 +8,9 @@ Clear-Host
 
 # https://github.com/mattparkes/PoShFuck
 try {
-	Import-Module PoShFuck
+	Import-Module PoShFuck -ErrorAction Stop
 }
 catch {
-	Set-ExecutionPolicy RemoteSigned
 	Invoke-Expression ((New-Object net.webclient).DownloadString('https://raw.githubusercontent.com/mattparkes/PoShFuck/master/Install-TheFucker.ps1'))
 	Import-Module PoShFuck
 }

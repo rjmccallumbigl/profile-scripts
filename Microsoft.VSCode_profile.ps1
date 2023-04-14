a6863bc688fb2b0a10b907314bd9c6c598da64d3
Ryan McCallum <rjmccallumbigl@gmail.com>
2023-04-13 21:25:52 -0400

    Update Microsoft.VSCode_profile.ps1 with new functions

--- Microsoft.VSCode_profile.ps1
+++ Microsoft.VSCode_profile.ps1
@@ -1,7 +1,13 @@
+# https://supportability.visualstudio.com/AzureIaaSVM/_wiki/wikis/AzureIaaSVM/495133/Powershell-Profile_RDP-SSH
+# set-alias WinGuestAnalyzer "\\fsu\shares\wats\scripts\WinGuestAnalyzer\WinGuestAnalyzer.ps1" -force -ErrorAction SilentlyContinue
+# Set-Alias hostanalyzer \\fsu\shares\wats\scripts\get-sub\hostanalyzer\hostanalyzer.ps1
+
+
 # https://github.com/PowerShell/PSReadLine
-try{ 
+try {
 	Import-Module PSReadLine
-} catch {
+}
+catch {
 	Write-Output "Skip import of PSReadline"
 }
 Clear-Host
@@ -16,9 +22,18 @@ catch {
 }
 
 # https://github.com/Chris--A/PowerLib/blob/main/Render-String.ps1
-C:\Users\Ryan\Documents\GitHub\PowerLib\Render-String.ps1 -In "RyanMcSloMo" -FontSize 12 -FontName Calibri -FontStyle Bold
+C:\Users\rymccall\GitHub\PowerLib\Render-String.ps1 -In "RyanMcSloMo" -FontSize 12 -FontName Calibri -FontStyle Bold
 
 # https://github.com/chubin/wttr.in
 # Invoke-RestMethod https://wttr.in/?F
-Invoke-RestMethod 'https://wttr.in?format=🌡️ %t (Feels like %f) %c(%C) 💧%h (humidity) 💨 %w ☂️  %p'
-Invoke-RestMethod 'https://wttr.in?format=🌄 %D 🔅 %S 🎇 %z 🌆 %s 🌃 %d %m \n'
+function Get-Weather {
+	Invoke-RestMethod 'https://wttr.in?format=🌡️ %t (Feels like %f) %c(%C) 💧%h (humidity) 💨 %w ☂️  %p'
+	Invoke-RestMethod 'https://wttr.in?format=🌄 %D 🔅 %S 🎇 %z 🌆 %s 🌃 %d %m \n'
+}
+Get-Weather
+
+# https://docs.microsoft.com/en-us/azure/azure-resource-manager/templates/test-toolkit
+Import-Module C:\users\RYMCCALL\GitHub\arm-ttk\arm-ttk\arm-ttk.psd1
+
+# https://github.com/rjmccallumbigl/Connect-RDPServer
+. "C:\Users\rymccall\OneDrive - Microsoft\PowerShell\Connect-RDPServer.ps1"
